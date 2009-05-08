04b55b299656018c6d9023a6df3a92a74326b4a9
robertkrimen <robertkrimen@gmail.com>
2009-05-08 08:56:54 -0700

    Documentation tweaking Added bash equivalent

--- lib/App/GitHub/FindRepository.pm
+++ lib/App/GitHub/FindRepository.pm
@@ -5,23 +5,23 @@ use strict;
 
 =head1 NAME
 
-App::GitHub::FindRepository 
+App::GitHub::FindRepository - Determine the right case (mixed or lower) for a GitHub repository
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
-    github-find-repository git://github.com/robertkrimen/DBIx-Deploy.git
-    # git://github.com/robertkrimen/dbix-deploy.git
+    github-find-repository git://github.com/robertkrimen/Doc-Simply.git
+    # git://github.com/robertkrimen/doc-simply.git
 
-    github-find-repository robertkrimen,DBIx-Deploy
-    # git://github.com/robertkrimen/dbix-deploy.git
+    github-find-repository robertkrimen,Doc-Simply
+    # git://github.com/robertkrimen/doc-simply.git
 
     github-find-repository --pinger=./bin/git-ls-remote ...
 
@@ -29,8 +29,8 @@ our $VERSION = '0.01';
 
     use App::GitHub::FindRepository
 
-    my $url = App::GitHub::FindRepository->find( git://github.com/robertkrimen/DBIx-Deploy.git )
-    # git://github.com/robertkrimen/dbix-deploy.git
+    my $url = App::GitHub::FindRepository->find( robertkrimen/Doc-Simply )
+    # git://github.com/robertkrimen/doc-simply.git
 
 =head1 DESCRIPTION
 
@@ -38,7 +38,65 @@ GitHub recently made a change that now allows mixed-case repository names. Unfor
 will not find the right repository given the wrong case.
 
 L<App::GitHub::FindRepository> (C<github-find-repository>) will first attempt to ping the mixed-case version, and,
-failing that, will attempt to ping the lowercase version. It will return/print the valid repository url, if any.
+failing that, will attempt to ping the lowercase version. It will return/print the valid repository url, if any
+
+=head1 CAVEAT
+
+Given a mixed-case repository, the find routine will try the mixed-case once, then the lowercase. It will not find anything
+else
+
+    github-find-repository robertkrimen/Doc-Simply
+
+...will work, as long as the real repository is C<robertkrimen/Doc-Simply.git> or C<robertkrimen/doc-simply.git>
+
+If the real repository is C<robertkrimen/dOc-sImPlY.git> then the finder will NOT see it
+
+...that being said, let me know if you want/need something that is more findy
+
+=head1 INSTALL
+
+    cpan -i App::GitHub::FindRepository
+
+You can also try using the bash script below if you need a quick-fix
+
+=head1 USAGE
+
+=head2 github-find-repository
+
+A commandline application that will print out the the repository with the right casing
+
+    Usage: github-find-repository [--pinger <pinger>] <repository>
+
+        --pinger        The pinger to use (default is either git-ls-remote or git-peek-remote)
+
+        <repository>    The repository to test, can be like:
+
+                        git://github.com/robertkrimen/App-GitHub-FindRepository.git
+                        robertkrimen/App-GitHub-FindRepository.git
+                        robertkrimen,App-GitHub-FindRepository
+
+=head2 $repository = AppGitHub::FindRepository->find( <repository> [, <pinger>] )
+
+Given a mixed-case repository URI, it will return the version with the right case
+
+=head2 A bash function as an alternative
+
+If you do not want to install App::GitHub::FindRepository, here is a bash equivalent:
+
+    #!/bin/bash
+
+    function github-find-repository() {
+        local pinger=`which git-ls-remote`
+        if [ "$pinger" == "" ]; then pinger=`which git-peek-remote`; fi
+        if [ "$pinger" == "" ]; then echo "Couldn't find pinger (git-ls-remote or git-peek-remote)"; return -1; fi
+        local repository=$1
+        if [ "`$pinger $repository 2>/dev/null`" ]; then echo $repository; return 0; fi
+        repository=`echo $repository | tr "[:upper:]" "[:lower:]" `
+        if [ "`$pinger $repository 2>/dev/null`" ]; then echo $repository; return 0; fi
+        return -1
+    }
+
+    github-find-repository $*
 
 =cut
 
